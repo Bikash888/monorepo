aa455ed92add734f3d8632055ef41205627cf509
dipesh <kcdipesh429@gmail.com>
2020-11-23 17:27:08 +0545

    fix:add codes to silent svg and next-image warning

--- packages/owner/next-env.d.ts
+++ packages/owner/next-env.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next-images" />
 
-// declare module "*.svg" {
-//     import React from "react";
-//     export default JSX.IntrinsicElements.svg;
-//   }
+declare module "*.svg" {
+    import React from "react";
+    export default JSX.IntrinsicElements.svg;
+  }
   
