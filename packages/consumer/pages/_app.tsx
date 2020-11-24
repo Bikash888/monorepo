3fa5d4a7b38905b6274fbc933a3ec1018e71b403
dipesh <kcdipesh429@gmail.com>
2020-11-24 18:04:56 +0545

    fix:minor bug

--- packages/consumer/pages/_app.tsx
+++ packages/consumer/pages/_app.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/router";
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
     enabled: process.env.NODE_ENV !== "development",
-    environment: `owner-${process.env.NODE_ENV}`,
+    environment: `consumer-${process.env.NODE_ENV}`,
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   });
 }
