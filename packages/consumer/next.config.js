d5f4eb776e833ba2e497376066c1c94b5a939002
dipesh <kcdipesh429@gmail.com>
2020-11-24 13:36:58 +0545

    refactor:add parent tsconfig

--- packages/consumer/next.config.js
+++ packages/consumer/next.config.js
@@ -1,6 +1,6 @@
 const withTM = require("next-transpile-modules")(["@project/shared"]);
 const withImages = require("next-images");
-const nextSourceMaps = require("@zeit/next-source-maps")();
+const nextSourceMaps = require("@zeit/next-source-maps");
 
 module.exports = withImages(
   nextSourceMaps(
