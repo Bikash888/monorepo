0d6b54bced97de91cb68bb6a2f34e6ba1eb8d373
dipesh <kcdipesh429@gmail.com>
2020-11-24 12:52:57 +0545

    feat:add sentry alias on next config

--- packages/owner/next.config.js
+++ packages/owner/next.config.js
@@ -1,4 +1,13 @@
 const withTM = require("next-transpile-modules")(["@project/shared"]);
 const withImages = require("next-images");
 
-module.exports = withImages(withTM());
+module.exports = withImages(
+  withTM({
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.alias["@sentry/node"] = "@sentry/browser";
+      }
+      return config;
+    },
+  })
+);
