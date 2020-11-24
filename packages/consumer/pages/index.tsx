54957fe4102fb74220bb8c197bf7235645988d9a
Aiska Basnet <aiska.basnet@gmail.com>
2020-11-24 16:50:53 +0545

    Can use svg as a component

--- packages/consumer/pages/index.tsx
+++ packages/consumer/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button } from "@project/shared";
 import { useTranslation } from "react-i18next";
+import PencilIcon from "../assets/pencil.svg";
 
 export default function Home() {
   const { t } = useTranslation();
   return (
     <>
       <Button>{t("Hello")}</Button>
+      <PencilIcon />
     </>
   );
 }
