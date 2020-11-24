2ebfa1dee115868be05af95d70e488ee34ae9842
Aiska Basnet <aiska.basnet@gmail.com>
2020-11-24 16:53:50 +0545

    Minor fix

--- packages/owner/pages/index.tsx
+++ packages/owner/pages/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "@project/shared";
 import About from "../assets/about2.png";
-import TopImageSrc from "../assets/top.svg";
 import { theme } from "@project/shared/src/theme";
 
 export default function Home() {
   return (
     <>
-      <Image height={150} width={150} src={TopImageSrc} loading="lazy" />
       <Image height={150} width={150} src={About} loading="lazy" />
       <Button minheight={50} bordercolor={theme.alert}>
         OWNER Button
