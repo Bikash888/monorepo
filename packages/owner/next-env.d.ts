0a67ffc7bcb42ade654ae38ee183045c783e4018
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-02 20:32:28 +0545

    Removed unused code from config

--- packages/owner/next-env.d.ts
+++ packages/owner/next-env.d.ts
@@ -2,7 +2,3 @@
 /// <reference types="next/types/global" />
 /// <reference types="next-images" />
 
-declare module "*.svg" {
-  import React from "react";
-  export default JSX.IntrinsicElements.svg;
-}
